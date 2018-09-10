1f82328d6fdef916686884f9a8338ec6a08fdcc7
José Moreira <diskoballs@gmail.com>
2018-09-10 17:02:04 +0100

    Update cookieSnippetJS.js

--- cookieSnippetJS.js
+++ cookieSnippetJS.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-const setPrivacy = function () {
-  localStorage.setItem("privacy", "read and agreed");
-}
-
-(()=>{
-  const value = localStorage.getItem("privacy");
-  //if not exists cookie
-  if (value === null) {
-    //do stuff ...
-    //set cookie.
-    setPrivacy();
+  const setPrivacy = function () {
+    localStorage.setItem("privacy", "read and agreed");
   }
-})();
+
+  const getPrivacy = function () {
+    const value = localStorage.getItem("privacy");
+    //if not exists cookie
+    if (value === null) {
+      //do stuff ...
+      //set cookie.
+      setPrivacy();
+    }
+  };
+  getPrivacy();
 
 });
